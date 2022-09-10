aa511bd10e1e0ed79a10d379c0850e9198ea899c
Cristian Canggianelli <canggianellicristian@gmail.com>
2022-09-10 20:23:21 -0300

    Se agrega comentario

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Curso(models.Model):
     nombre = models.CharField(max_length=40)                #Formato str con max de carcateres
-    camada = models.IntegerField()
+    camada = models.IntegerField()                          #Formato numerico
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)                #Formato str con max de carcateres
